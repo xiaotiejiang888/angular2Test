e1858e58697f38faceaa810390d0177f1e765e60
maoyu.zhang <maoyu.zhang@tendcloud.com>
2017-02-18 21:10:47 +0800

    路由-步骤6-增加routing模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import {HeroService} from "./hero.service";
 import {AppComponent} from "./app.component"
-import {RouterModule} from "@angular/router"
 import {DashboardComponent} from "./dashboard.component"
-
+import {AppRoutingModule} from "./app-routing.module"
 @NgModule({
   declarations: [
     HeroesComponent,
     HeroDetailComponent,
-    HeroesComponent,
     DashboardComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    RouterModule.forRoot([
-      {
-        path:'heroes',
-        component:HeroesComponent
-      },{
-        path: 'dashboard',
-        component: DashboardComponent
-      },{
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },{
-        path:'detail/:id',
-        component:HeroDetailComponent
-      }
-    ])
+    AppRoutingModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
